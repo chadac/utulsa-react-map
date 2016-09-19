69ad20a9a98df087ab4e185354af7e40dc35e9f9
Chad Crawford <chad-crawford@utulsa.edu>
2016-09-19 13:15:42 -0500

    Moving burger menu to the right, some stylizing and showing item entries.
    `react-burger-menu` passes styles via JS, so I can't store the class
    information in a separate SASS module as I had been doing earlier. For
    now, I use the dictionary object provided in the documentation.

--- src/components/Listing.jsx
+++ src/components/Listing.jsx
@@ -1,27 +1,34 @@
 import React, {Component} from 'react';
+import ItemStore from '../stores/ItemStore';
+import classnames from 'classnames';
 import BurgerMenu from 'react-burger-menu';
+import styles from '../stylesheets/Listing.scss';
 
+// TODO: Figure out how to import styles in this format using webpack's
+//       css-loader.
 const BurgerStyles = {
   bmBurgerButton: {
     position: 'fixed',
     width: '36px',
     height: '30px',
-    left: '36px',
+    left: 'initial',
+    right: '36px',
     top: '36px'
   },
   bmBurgerBars: {
-    background: '#373a47'
+    background: 'rgb(0, 70, 100)'
   },
   bmCrossButton: {
     height: '24px',
-    width: '24px'
+    width: '24px',
+    marginRight: '10px',
   },
   bmCross: {
-    background: '#bdc3c7'
+    background: 'rgb(255, 255, 255)',
   },
   bmMenu: {
-    background: '#373a47',
-    padding: '2.5em 1.5em 0',
+    background: 'rgb(0, 75, 141)',
+    padding: '2.5em 0.5em 0',
     fontSize: '1.15em'
   },
   bmMorphShape: {
@@ -36,17 +43,51 @@ const BurgerStyles = {
   }
 }
 
+const MenuWrapper = React.createClass({
+  render() {
+    return (
+      <div className={classnames(styles.menuWrapper)}>
+        {this.props.children}
+      </div>
+    );
+  },
+});
+
+const SearchBar = React.createClass({
+  render() {
+    return (
+      <input className={classnames(styles.searchBar)} type="text" />
+    );
+  }
+});
+
+const SearchItem = React.createClass({
+  render() {
+    return (
+      <a href="#" className={classnames(styles.searchItem)}>
+        <span>{this.props.name}</span>
+      </a>
+    );
+  }
+});
+
 const Listing = React.createClass({
   render() {
     const Menu = BurgerMenu['slide'];
+    const searchItems = ItemStore.getAll().map((item) => (
+      <SearchItem {...item} />
+    ));
     return (
-      <Menu pageWrapId={this.props.pageWrapId}
-            outerContainerId={this.props.outerContainerId}
-            styles={BurgerStyles}
-            wait={20}
-            left noOverlay>
-        <form><input type="text" /></form>
-      </Menu>
+      <MenuWrapper>
+        <Menu pageWrapId={this.props.pageWrapId}
+              outerContainerId={this.props.outerContainerId}
+              styles={BurgerStyles}
+              wait={20}
+              right noOverlay>
+          <SearchBar />
+          {searchItems}
+        </Menu>
+      </MenuWrapper>
     );
   },
 });
